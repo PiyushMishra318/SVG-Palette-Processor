e67ce29701dc0c0db09ebfd7912c57c38ddf8329
Saurabh Sabharwal <saurabh@codeacious.tech>
2020-02-14 12:23:42 +0530

    Output json now has rgba object instead of hex code of color

--- index.js
+++ index.js
@@ -72,12 +72,28 @@ function createDefElement(document, color, id) {
     return linearGradient;
 }
 
+function hexToRgb(hex) {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+        a: 1
+    } : null;
+}
+
 function processPallette(pallette) {
     var result = {}
     var regExp = /\(([^)]+)\)/;
     var matches = regExp.exec("I expect five hundred dollars ($500).");
     for (var key in pallette) {
-        result[key] = regExp.exec(pallette[key])[1]
+        result[regExp.exec(pallette[key])[1]] = JSON.stringify(hexToRgb(key))
     }
     return result
 }
