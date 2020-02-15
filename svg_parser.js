f9dd6821c42ce51d3ab01dd5b96fd4575c14ab68
Piyush Mishra <piyush_mishra31@outlook.com>
2020-02-15 14:48:17 +0530

    parser updated

--- svg_parser.js
+++ svg_parser.js
@@ -12,16 +12,7 @@ if (process.argv[2]) {
             var { fills, strokes } = solid_cols;
             var gradients = findGradientColorsSvgElems(dom.window.document);
             var { stops } = gradients;
-            var svg_elem = dom.window.document.getElementsByTagName('svg')[0];
-            svg_elem.removeAttribute("width");
-            svg_elem.removeAttribute("height");
-            var updated_svg_path = "updated_svg.svg";
-            fs.writeFileSync(updated_svg_path, svg_elem.outerHTML);
-            fs.writeFileSync("svg_json.json", JSON.stringify({ fills, strokes, stops }))
-            console.log({
-                output_svg: __dirname + "\\" + updated_svg_path,
-                output_json: __dirname + "\\" + "svg_json.json"
-            });
+            console.log(fills, strokes, stops)
         }
         else {
             console.log("Invalid file Path");
@@ -64,7 +55,10 @@ function findGradientColorsSvgElems(document) {
     gradient_tag_list.forEach((tag) => {
         var gradients = document.getElementsByTagName(tag);
         for (var gradient of gradients) {
+            var id = `${tag}${++i}`;
+            gradient.setAttribute("id", id);
             var stops = gradient.getElementsByTagName("stop")
+            console.log(stops.length)
             if (stops && stops.length > 0) {
                 var stop_list = []
                 for (var stop of stops) {
@@ -72,7 +66,7 @@ function findGradientColorsSvgElems(document) {
                         && stop.getAttribute("stop-color") != "none"
                         && stop.getAttribute("stop-color") != ""
                         && !stop.getAttribute("stop-color").includes("url")) {
-                        var id = `stop${++j}`;
+                        var id = `${tag}${++j}`;
                         var check = checkForExist(tag, convertToRGBA(stop.getAttribute("stop-color")), stop_list)
                         if (!check.hasOwnProperty("id")) {
                             stop.setAttribute("id", id);
@@ -88,7 +82,7 @@ function findGradientColorsSvgElems(document) {
                         && stop.style.getPropertyValue("stop-color") != ""
                         && stop.style.getPropertyValue("stop-color") != "none"
                         && !stop.style.getPropertyValue("stop-color").includes("url")) {
-                        var id = `stop${++j}`;
+                        var id = `${tag}${++j}`;
                         var check = checkForExist(tag, convertToRGBA(stop.style.getPropertyValue("stop-color")), stop_list)
                         if (!check.hasOwnProperty("id")) {
                             stop.setAttribute("id", id);
@@ -106,12 +100,12 @@ function findGradientColorsSvgElems(document) {
                 }
                 if (stop_list.length > 0) {
                     result_stops.push({
-                        id: gradient.getAttribute("id"),
+                        id: id,
                         stops: stop_list
                     });
                 }
             } else {
-                console.log(`${gradient.getAttribute("id")} has no stop(s)`)
+                console.log(`${tag}${++i} has no stop(s)`)
             }
         }
     })
@@ -128,10 +122,6 @@ function findSolidColorSvgElems(document) {
         var elems = document.getElementsByTagName(tag)
         for (var elem of elems) {
             // can be improved furthur
-            console.log(elem.style.hasOwnProperty("fill")
-                , elem.style.getPropertyValue("fill")
-                , elem.style.getPropertyValue("fill") != "none"
-                , !elem.style.getPropertyValue("fill").includes("url"))
             if (elem.hasAttribute("fill")
                 && elem.getAttribute("fill") != "none"
                 && elem.getAttribute("fill") != ""
@@ -288,128 +278,3 @@ function convertToRGBA(color) {
     console.log(rgba)
     return rgba;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const getColors = require("get-svg-colors");
-// const tools = require('simple-svg-tools');
-// const jsdom = require("jsdom");
-// const { JSDOM } = jsdom;
-// const fs = require('fs');
-
-
-// // verify the input file argument
-// if (process.argv[2]) {
-//     fs.exists(`${process.argv[2]}`, (bool) => {
-//         if (bool) {
-//             tools.ImportSVG(`${process.argv[2]}`).then(svg => {
-//                 var dom = new JSDOM(svg.toString());
-//                 var document = dom.window.document;
-//                 var colors = []
-//                 tools.GetPalette(svg).then(result => {
-//                     colors = result.colors;
-//                     var { new_pallette, defs } = generateDefObj(document, colors);
-//                     tools.ChangePalette(svg, new_pallette).then(svg => {
-//                         var new_dom = new JSDOM(svg.toString());
-//                         var svg_elem = new_dom.window.document.getElementsByTagName('svg')[0];
-//                         svg_elem.removeAttribute("width");
-//                         svg_elem.removeAttribute("height");
-//                         svg_elem.appendChild(defs);
-//                         var updated_svg_path = "updated_svg.svg";
-//                         fs.writeFileSync(updated_svg_path, svg_elem.outerHTML);
-//                         var output_colors = processPallette(new_pallette)
-//                         console.log(JSON.stringify({ new_pallette: output_colors, svg: svg_elem.outerHTML, updated_svg_path: __dirname + "\\" + updated_svg_path }));
-//                     }).catch(err => {
-//                         console.log(err);
-//                     });
-//                 }).catch((err) => {
-//                     console.log(err)
-//                 })
-
-//             }).catch(err => {
-//                 console.log(err);
-//             });
-//         }
-//         else {
-//             console.log("Invalid file Path");
-//         }
-//     });
-// } else {
-//     console.log("Pass the file path as the third argument\n");
-//     console.log("For eg: node index input.svg (input.svg exists in the root folder)");
-// }
-
-// //util functions
-// function generateDefObj(document, list) {
-//     var result = {};
-//     var i = 0;
-//     var defs = document.createElementNS("http://www.w3.org/2000/svg", 'defs');
-//     list.forEach((color) => {
-//         result[color] = `url(#custom-color-${++i})`;
-//         defs.appendChild(createDefElement(document, color, `custom-color-${i}`));
-//     })
-//     var resp = { new_pallette: result, defs: defs };
-//     return resp;
-// }
-
-
-// function createDefElement(document, color, id) {
-//     var linearGradient = document.createElementNS("http://www.w3.org/2000/svg", 'linearGradient');
-//     linearGradient.setAttribute("id", id);
-//     var stop = document.createElementNS("http://www.w3.org/2000/svg", 'stop');
-//     stop.setAttribute("stop-color", color);
-//     stop.setAttribute("offset", "100%");
-//     linearGradient.appendChild(stop);
-//     return linearGradient;
-// }
-
-// function hexToRgb(hex) {
-//     // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-//     var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-//     hex = hex.replace(shorthandRegex, function (m, r, g, b) {
-//         return r + r + g + g + b + b;
-//     });
-
-//     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-//     return result ? {
-//         r: parseInt(result[1], 16),
-//         g: parseInt(result[2], 16),
-//         b: parseInt(result[3], 16),
-//         a: 1
-//     } : null;
-// }
-
-// function processPallette(pallette) {
-//     var result = {}
-//     var regExp = /\(([^)]+)\)/;
-//     var matches = regExp.exec("I expect five hundred dollars ($500).");
-//     for (var key in pallette) {
-//         result[regExp.exec(pallette[key])[1]] = JSON.stringify(hexToRgb(key))
-//     }
-//     return result
-// }
